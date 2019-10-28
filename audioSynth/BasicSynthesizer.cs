c678bd92b101b077e0eb61553bd74ebb1a3de7d1
Zachary Steiger <zsteig93@gmail.com>
2019-10-28 09:58:04 -0400

    Initial commit

---
+++ audioSynth/BasicSynthesizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace audioSynth
+{
+	public partial class BasicSynthesizer : Form
+	{
+		private const int SAMPLE_RATE = 44100;
+		public BasicSynthesizer()
+		{
+			InitializeComponent();
+		}
+
+		private void BasicSynthesizer_KeyDown(object sender, KeyEventArgs e)
+		{
+			MessageBox.Show("detected");
+		}
+	}
+}
