486506902c0d74129df295b9d10a0b64033834a2
Zachary Steiger <zsteig93@gmail.com>
2019-10-28 12:46:27 -0400

    Quick edit, added Oscillator class

--- audioSynth/BasicSynthesizer.cs
+++ audioSynth/BasicSynthesizer.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
+using System.IO;
 
 namespace audioSynth
 {
@@ -26,8 +27,10 @@ namespace audioSynth
 		{
 			// Samples stored into an array
 			short[] wave = new short[SAMPLE_RATE];
+			// need to convert into binary wave to be read
+			byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)];
 			// Example frequency for testing
-			float frequency = 440f;
+			float frequency = 220f;
 			/*
 				Sin Loop. Sample = Amplitude * sin(t * i) where t is angular frequency, i is unit of time 
 			*/
@@ -35,6 +38,33 @@ namespace audioSynth
 			{
 				wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i));
 			}
+			//split each short into 2 bytes and write them into the binary wave
+			Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
+			// The next section follows the WAV format standards to play audio
+			using (MemoryStream memoryStream = new MemoryStream())
+			using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+			{
+				short blockAlign = BITS_PER_SAMPLE / 8;
+				int subChunkTwoSize = SAMPLE_RATE * blockAlign;
+				// make an array for each individual character following ASCII specifications
+				binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+				binaryWriter.Write(36 + subChunkTwoSize);
+				binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+				binaryWriter.Write(16);
+				binaryWriter.Write((short)1);
+				binaryWriter.Write((short)1);
+				binaryWriter.Write(SAMPLE_RATE);
+				binaryWriter.Write(SAMPLE_RATE * blockAlign);
+				binaryWriter.Write(blockAlign);
+				binaryWriter.Write(BITS_PER_SAMPLE);
+				binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+				binaryWriter.Write(subChunkTwoSize);
+				binaryWriter.Write(binaryWave);
+				// set memoryStream to 0 so it reads the start of the sound instead of the where it left off
+				memoryStream.Position = 0;
+				new SoundPlayer(memoryStream).Play();
+			}
+
 		}
 	}
 }
